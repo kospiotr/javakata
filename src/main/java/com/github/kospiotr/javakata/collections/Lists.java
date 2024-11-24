dff66b9c583263c0ab4d32b255dc9a0f7b5cddfb
Piotr Kosmowski <kospiotr@gmail.com>
2024-11-24 14:20:01 +0100

    Update

---
+++ src/main/java/com/github/kospiotr/javakata/collections/Lists.java
@@ -0,0 +1,12 @@
+package com.github.kospiotr.javakata.collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Lists {
+
+    public void arrayList(){
+        new ArrayList<String>();
+        HashSet<String>();
+    }
+}
